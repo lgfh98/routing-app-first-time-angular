c47d0ad72c87c98fa76cb22c17b425559ae0515e
lgfh98 <lgfigueroah@outlook.com>
2022-06-24 15:54:17 -0500

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { HttpClientModule } from '@angular/common/http';
 import { TestComponent } from './test/test.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, TopBarComponent, TestComponent, ProductAlertsComponent, ProductDetailsComponent],
-  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    TestComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
